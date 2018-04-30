b3c674fd9d9499f712e149b9e71812cbf9250433
SDKR <ecgk@live.dk>
2018-04-30 11:05:57 +0200

    Withdraw

--- H2_Case_Bank/H2_Case_Bank/DBAccount.cs
+++ H2_Case_Bank/H2_Case_Bank/DBAccount.cs
@@ -1,21 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace H2_Case_Bank
 {
     class DBAccount
     {
 
+        string constring = @"server=DESKTOP-5QOPHSN\SQLOPG;database=Bank;UID=sa;password=Wak40336";
+        //Kims sql login
+        //string constring = @"server=SKAB4-PC-01\KIM;database=Bank;UID=sa;password=Pa$$w0rd";
+        //Kims sql login 
+        //string constring = @"server=SKAB4-PC-03;database=Bank;UID=sa;password=Passw0rd";
+
         public void Withdraw(int accountnumber, double transaction)
         {
-            var connetionString = "Data Source=EVOPC18\\PMSMART;Initial Catalog=NORTHWND;User ID=test;Password=test";
-            var sql = "UPDATE Employees SET LastName = @LastName, FirstName = @FirstName, Title = @Title ... ";// repeat for all variables
+
+            SqlConnection sqlConn = new SqlConnection(constring);
+            SqlCommand cmd = new SqlCommand("Select Balance from Account where PK_Accountnumber = "+ accountnumber +" ", sqlConn);
+            sqlConn.Open();
+            SqlDataAdapter adapt = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adapt.Fill(ds);
+            sqlConn.Close();
+
+            double currBalance = double.Parse(ds.Tables[0].Rows[0].ToString());
+            Console.WriteLine(currBalance);
+
+                /*
+            var sql = "UPDATE Account SET = @LastName, FirstName = @FirstName, Title = @Title ... ";// repeat for all variables
             try
             {
-                using (var connection = new SqlConnection(connetionString))
+                using (var connection = new SqlConnection(constring))
                 {
                     using (var command = new SqlCommand(sql, connection))
                     {
@@ -32,6 +53,8 @@ namespace H2_Case_Bank
             {
                 MessageBox.Show($"Failed to update. Error message: {e.Message}");
             }
+
+            */
         }
 
     }
